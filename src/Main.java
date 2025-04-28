6842686a259fd5995bf7326e520a06599ea3e8c2
artem-yanishev <janyshevtema79@gmail.com>
2025-04-28 15:14:15 +0300

    homework6

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,3 @@
-class Main2 {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("Итерация цикла " + i);
-        }
-        for (int i = 2020; i <= 2070; i = i + 4) {
-            System.out.println("Високосный год " + i);
-        }
-        int salary = 65535;
-        int total = 0;
-        for (int i = 0; i < 12; i++) {
-            total = total + total / 100;
-            total = total + salary;
-            System.out.println("Месяц " + (i + 1) + " Итого " + total);
-        }
-        System.out.println(total);
-    }
-}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1.");
